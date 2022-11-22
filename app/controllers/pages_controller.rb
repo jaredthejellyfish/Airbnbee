3ea25d22eab79725d6298f32387bd3238f69fa22
Gerard Hernandez <ger.almenara@gmail.com>
2022-11-22 19:22:41 +0100

    --

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -2,6 +2,6 @@ class PagesController < ApplicationController
   skip_before_action :authenticate_user!, only: [:home]
 
   def home
-    @insects = Insect.all # all them insects (ignore me, im a comment necessary to push to Heroku, I'll be removed shortly)
+    @insects = Insect.all
   end
 end
