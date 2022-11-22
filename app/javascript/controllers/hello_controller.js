688192d2e0d5deea305db622aa78ac46b9fa381c
Gerard Hernandez <ger.almenara@gmail.com>
2022-11-22 15:06:53 +0100

    --

--- app/javascript/controllers/hello_controller.js
+++ app/javascript/controllers/hello_controller.js
@@ -5,3 +5,9 @@ export default class extends Controller {
     this.element.textContent = "Hello World!"
   }
 }
+
+const fetch_insects = () => {
+  fetch('http://localhost:3000/api/insects')
+    .then((response) => response.json())
+    .then((data) => data['data']);
+}
