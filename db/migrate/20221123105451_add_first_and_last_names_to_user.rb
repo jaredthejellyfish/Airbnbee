31711818b7148ca32388bab32d3314ddc921ab49
Gerard Hernandez <ger.almenara@gmail.com>
2022-11-23 12:28:15 +0100

    Modified migration to accoutn for issues with first and last name data types

--- db/migrate/20221123105451_add_first_and_last_names_to_user.rb
+++ db/migrate/20221123105451_add_first_and_last_names_to_user.rb
@@ -1,6 +1,6 @@
 class AddFirstAndLastNamesToUser < ActiveRecord::Migration[7.0]
   def change
-    change_column :users, :first_name, :string
-    change_column :users, :last_name, :string
+    add_column :users, :first_name, :string
+    add_column :users, :last_name, :string
   end
 end
