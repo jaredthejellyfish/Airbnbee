d995c23d8c15adf54693077e4bd422d8d6c90346
grant chichester <gchichester@gmail.com>
2022-11-23 14:19:56 +0100

    made some changes to users

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -16,7 +16,7 @@ class UsersController < ApplicationController
   private
 
   def user_params
-    params.require(:user).permit(:first_name, :last_name, :email)
+    params.require(:user).permit(:first_name, :last_name, :email, :photo)
   end
 
 
