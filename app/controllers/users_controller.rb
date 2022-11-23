b2b4cfa985507fccd6a058d9e45419853c8195e8
Francois Lillet <francois.lillet@hec.edu>
2022-11-23 15:21:49 +0100

    Worked on the pictures profile

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,7 +1,6 @@
 class UsersController < ApplicationController
   def show
     @user = current_user
-    raise
   end
 
   def edit
@@ -18,8 +17,7 @@ class UsersController < ApplicationController
   private
 
   def user_params
-    params.require(:user).permit(:first_name, :last_name, :email)
+    params.require(:user).permit(:first_name, :last_name, :email, :photo)
   end
-
 
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -1,4 +1,8 @@
+require "open-uri"
+
 class User < ApplicationRecord
+  has_one_attached :photo
+
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
@@ -6,4 +10,10 @@ class User < ApplicationRecord
 
   has_many :insects, through: :bookings
   has_many :bookings
+
+  # def initialize(attributes = {})
+  #   file = URI.open("https://i0.wp.com/ifpo.org.uk/wp-content/uploads/2020/10/cropped-9-93879_computer-icons-user-image-person-silhouette-user-silhouettes-5.png?ssl=1")
+  #   self.photo.attach(io: file, filename: "avatar.png", content_type: "image/png")
+  # end
+
 end
