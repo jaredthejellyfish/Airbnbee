be9fc88fdcc94d8cf05789894bdf47e6c8f3f6f6
grant chichester <gchichester@gmail.com>
2022-11-24 17:41:35 +0100

    bio working and address field added

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -16,6 +16,6 @@ class UsersController < ApplicationController
   private
 
   def user_params
-    params.require(:user).permit(:first_name, :last_name, :email, :photo)
+    params.require(:user).permit(:first_name, :last_name, :email, :bio, :photo)
   end
 end
