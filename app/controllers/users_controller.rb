06e0c9d4568997db58068b4b8f4daf5cfe165975
Gerard Almenara <ger.almenara@gmail.com>
2022-11-24 16:58:19 +0100

    Revert "added bio to user"
    This reverts commit e1d91800c21ebe85703d642efa45f928d4b0bd8b.
    
    alright, fixed ?

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -16,6 +16,6 @@ class UsersController < ApplicationController
   private
 
   def user_params
-    params.require(:user).permit(:first_name, :last_name, :email, :bio, :photo)
+    params.require(:user).permit(:first_name, :last_name, :email, :photo)
   end
 end
